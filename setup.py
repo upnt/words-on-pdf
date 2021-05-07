21e1b124e62a246e6fef706451ff6d870da72865
upnt <upnt.github@gmail.com>
2021-05-07 12:37:03 +0900

    setup for project included test

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+ 
+setup(
+    name="words_on_pdf",
+    version="1.0",
+    license="LICENSE",
+    description="clip words on pdf",
+    author="upnt",
+    url="https://github.com/upnt/words_on_pdf",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"]
+)
